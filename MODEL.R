c868741a70e6ba375920d406f2a1911d85401d78
dreeux <arunpavuri@gmail.com>
2015-11-10 18:55:27 +0530

    edit

--- MODEL.R
+++ MODEL.R
@@ -43,7 +43,7 @@ clf <- xgb.train(params = param, data = dtrain, nrounds = 500, watchlist = watch
 
 #normal training method
 
-feature.names <- names(train)
+feature.names <- names(train)[-179]
 
 tra <- train[, feature.names]
 
@@ -70,7 +70,7 @@ param <- list(objective = "multi:softprob",
               num_class = numberOfClasses,
               
               nthreads = detectCores()
-)
+              )
 
 gc()
 
@@ -87,6 +87,7 @@ clf <- xgb.train(params = param, data = dtrain, nrounds = 50, watchlist = watchl
                  
                  verbose = 1, maximize = T)
 
+time_taken <- Sys.time() - start
 
 #############################################################################################################
 
@@ -135,7 +136,29 @@ Time_Taken <- Sys.time() - start
 
 ##after 900 rounds it increases then falls off check it`s behaviour further
 
-submit(clf, test, "1172015.csv")
+# NOT USING THE SUBMISSION FUNCTION PRED FUNCTION FOR NOW 11-10-2015
+
+#submit(clf, test, "1172015.csv")
+
+pred <- predict(clf, data.matrix(test[, feature.names])) 
+
+pred <- matrix(pred, nrow=38, ncol=length(pred)/38) #there are total 38 classes 
+
+pred <-  data.frame(t(pred))
+
+sample <- read_csv("D:/kaggle/walmart_seg/Data/sample_submission.csv") 
+
+cnames <- names(sample)[2:ncol(sample)] 
+
+names(pred) <- cnames
+
+submission <- cbind.data.frame(VisitNumber = visit_num , pred) 
+
+submission <- setDT(submission)
+
+submission <- (submission[ , lapply(.SD, mean), by = VisitNumber])
+
+write_csv(submission, "D:/kaggle/walmart_seg/submission/11102015.csv")
 
 ####################################################################################################################
 
