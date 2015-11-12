22370bdb683c86f5b242505deec11a18abb6fd53
dreeux <arunpavuri@gmail.com>
2015-11-12 22:00:32 +0530

    edit

--- model.R
+++ model.R
@@ -1,175 +1,555 @@
-#feature hashing
+# explicitly mention data.table usage at the beginning 
 
-# didn`t show much of a difference in eval_metric for50 trees and default param
+#load packages
 
-train_hash <- train
+require(xgboost); require(readr); require(caret); require(doParallel); require(data.table); require(FeatureHashing); require(sqldf); require(dplyr)
 
-train_hash[is.na(train_hash)] <- 0
+set.seed(11102015)
 
-split <- createDataPartition(y = train_hash$TripType, p = 0.9, list = F) 
+################################################################################################################
 
-training <- train_hash[split,]
+#read in data 
 
-validation <- train_hash[-split,]
+#train_raw <- read_csv("C:/Users/amulya/Documents/Kaggle/Walmart/train.csv")
 
-training_hash = hashed.model.matrix(~., data=training[,feature.names],  hash.size=2^16,  
-                                    
-                                    transpose=FALSE, create.mapping=TRUE, is.dgCMatrix = TRUE)
+#test_raw <- read_csv("C:/Users/amulya/Documents/Kaggle/Walmart/test.csv")
 
-validation_hash = hashed.model.matrix(~., data=validation[,feature.names],  hash.size=2^16,  
-                                      
-                                      transpose=FALSE, create.mapping=TRUE, is.dgCMatrix = TRUE)
+train_raw <- read_csv("D:/kaggle/walmart_seg/Data/train.csv")
 
-response_val <- train_hash$TripType[-split]
+test_raw <- read_csv("D:/kaggle/walmart_seg/Data/test.csv")
 
-response_train <- train_hash$TripType[split]
+visit_num <- test_raw$VisitNumber
 
-dval <- xgb.DMatrix(data=validation_hash, label = response_val )
+################################################################################################################
 
-dtrain <- xgb.DMatrix(data=training_hash,  label = response_train)
+feature.names <- names(train_raw)[!names(train_raw) %in% c("TripType")]
 
-watchlist <- list(val=dval, train=dtrain)
+response <- names(train_raw)[1]
 
-clf <- xgb.train(params = param, data = dtrain, nrounds = 500, watchlist = watchlist,
-                 
-                 verbose = 1, maximize = T)
+response <- train_raw[ , response] 
+
+class_old <- sort(unique(response$TripType))
+
+class_new <- seq(0, 37)
+
+#replace elements of class_old with elements of class_new in response
+
+for( i in 1:38 ){
+  
+  train_raw$TripType[train_raw$TripType == class_old[i]] <- class_new[i]
+  
+}
+
+table(train_raw$TripType)
+
+################################################################################################################
+
+
+#very basic stuff----------------------------------------------------------------------------------------------
+
+# options : 1. find better ways of extracting info from text variables
+
+#           2. one way is stuff such as count, tf idf etc...
+
+#           3. there are other wayss which as of 11072015 I dnt know but I will refer @Kaggle
+
+#           4. Below mentioned is a very basic way of using character information.
+
+
+cat("assuming text variables are categorical & replacing them with numeric ids\n")
+
+for (f in feature.names) {
+  
+  if (class(train_raw[[f]]) == "character") {
+    
+    levels <- unique(c(train_raw[[f]], test_raw[[f]]))
+    
+    train_raw[[f]] <- as.integer(factor(train_raw[[f]], levels=levels))
+    
+    test_raw[[f]]  <- as.integer(factor(test_raw[[f]],  levels=levels))
+  }
+}
+
+
+###############################################################################################################
+
+# combine train and test
+
+# easier way to manipulate data for both train and test
+
+# columns in both test and train should be the same for training , testing , validation and prediction
 
 
 
+tmp <- rbind(train_raw[ , feature.names], test_raw)
+
+# IMP : FACED ISSUES [ READ : CODE BREAKS] WHEN tbl_df WAS PRESENT CLASS
+
+#     : as.factor CONVERSION COULDN`T GO THROUGH 
+
+#     : REMOVING THE CLASS PROVED BENEFICIAL ; RUN THE BELOW CODE FOR CONVERSION
+
+tmp <- data.frame(tmp)
+
+
+# for counts (1 - way, 2 - way, 3 - way)
+
+# selected columns should contain factors
+
+# so first select columns and then convert them into factors  
+
+# Also maintain name used in below code for easy reproducibilty ( use the same names for ur present df)
 
 
 
-#################################################################################################
+tmp_factors = tmp[ , feature.names]; dim(tmp_factors)
 
-#normal training method
+len = length(names(tmp_factors))
 
-feature.names <- names(train)[-179]
+for (i in 1:len) {
+  
+  print(paste0( i / (len) *100, "%"))
+  
+  tmp_factors[ , i] <- as.factor(tmp_factors[ , i])
+  
+}
 
-tra <- train[, feature.names]
+#Important step ^^^^
 
-split <- createDataPartition(y = train_raw$TripType, p = 0.9, list = F) 
+#############################################################################################################
 
-response_val <- train_raw$TripType[-split]
 
-response_train <- train_raw$TripType[split]
+# 2 way count
 
-dval <- xgb.DMatrix( data = data.matrix(tra[-split,]),  label = response_val )
+nms <- combn(names(tmp_factors), 2)
 
-dtrain <- xgb.DMatrix( data = data.matrix(tra[split,]), label = response_train)
+dim(nms)
 
-watchlist <- list(val=dval, train=dtrain)
+nms_df <- data.frame(nms) 
 
-#basic training----------------------------------------------------------------------------------
+len = length(names(nms_df))
 
-numberOfClasses <- max(train_raw$TripType) + 1
+for (i in 1:len) {
+  
+  nms_df[, i] <- as.character(nms_df[, i])
+  
+}
 
-param <- list(objective = "multi:softprob",
-              
-              eval_metric = "mlogloss",
-              
-              num_class = numberOfClasses,
-              
-              max_depth = 12,
-              
-              eta = 0.01,
-              
-              colsample_bytree = 0.8,
-              
-              subsample = 0.8
-              
-              )
+tmp_count <- data.frame(id = 1:dim(tmp)[1])
+
+for(i in 1:dim(nms_df)[2]){
+  
+  
+  #new df 
+  
+  print(((i / dim(nms_df)[2]) * 100 ))
+  
+  tmp_count[, paste(i, "_two", sep="")] <- my.f2cnt(th2 = tmp, 
+                                                    
+                                                    vn1 = nms_df[1,i], 
+                                                    
+                                                    vn2 = nms_df[2,i] )
+  
+}
+
+###############################################################################################################
+
+
+
+#3 way count
+
+nms <- combn(names(tmp_factors), 3)
+
+dim(nms)
+
+nms_df <- data.frame(nms);
+
+len = length(names(nms_df))
+
+for (i in 1:len) {
+  
+  print(paste0(( i / len) *100, "%"))
+  
+  nms_df[, i] <- as.character(nms_df[, i])
+  
+}
+
+for(i in 1:dim(nms_df)[2]){
+  
+  #new df 
+  
+  print((i / dim(nms_df)[2]) * 100)
+  
+  tmp_count[, paste(i, "_three", sep="")] <- my.f3cnt(th2 = tmp, 
+                                                      
+                                                      vn1 = nms_df[1,i], 
+                                                      
+                                                      vn2 = nms_df[2,i], 
+                                                      
+                                                      vn3 = nms_df[3,i])
+  
+}
+
+
+##############################################################################################################
+
+
+
+#one way count
+
+len = dim(tmp_factors)[2]
+
+for(i in 1:len){
+  
+  
+  print((i / len) * 100 )
+  
+  tmp_factors$x <- tmp_factors[, i]
+  
+  sum1 <- sqldf("select x, count(1) as cnt
+                
+                from tmp_factors  group by 1 ")
+  
+  tmp1 <- sqldf("select cnt from tmp_factors a left join sum1 b on a.x=b.x")
+  
+  tmp_count[, paste(names(tmp_factors)[i], "_one", sep="")] <- tmp1$cnt
+  
+}  
+
+###############################################################################################################
+
+
+# IMP : This step is not required unless the method of creating tmp_factors is similar to 
+
+# that used in springleaf Marketing response challenge
+
+# for this contest skipping this step as it is not required
+
+
+
+#need to convert all of tmp_factors to numeric
+
+
+len = length(names(tmp_factors))
+
+for (i in 1:len) {
+  
+  tmp_factors[, i] <- as.numeric(tmp_factors[, i])
+  
+}
+
+
+
+###############################################################################################################
+
+
+
+#use this code before running dummy variables conversion 
+
+#Note IMP:  1. Selected columns should be of factors
+
+
+#dummify few columns
+
+
+name <- c("Weekday", "DepartmentDescription", "ScanCount")
+
+tmp_dummy <- (tmp[ , name ])
+
+len = length(names(tmp_dummy))
+
+for (i in 1:len) {
+  
+  print(paste0(( i / len) * 100, "%"))
+  
+  tmp_dummy[, i] <- as.factor(tmp_dummy[, i])
+  
+}
+
+
+
+dummies <- dummyVars( ~ ., data = tmp_dummy)
 
 gc()
 
+tmp_dummy <- predict(dummies, newdata = tmp_dummy)
 
-cl <- makeCluster(detectCores()); registerDoParallel(cl)
+tmp_dummy <- data.frame(tmp_dummy)
 
+dim(tmp_dummy)
 
-start <- Sys.time()
-
-#############################################################################################################
-
-
-clf <- xgb.train(params = param, data = dtrain, nrounds = 50, watchlist = watchlist,
-                 
-                 verbose = 1, maximize = T, nthread = 2)
-
-time_taken <- Sys.time() - start
-
-#############################################################################################################
+##############################################################################################################
 
 
-#grid search
+#test if the below code works before running 
 
-for (depth in c(9, 10, 8)) {
+
+tmp_features <- data.frame(id = 1:dim(tmp)[1])
+
+
+
+# features for difference from mean
+
+for(i in 1:ncol(tmp_new)){
   
-  for (rounds in c(2000, 3000)) {
+  tmp_features[, paste(names(tmp_new)[i], "_mean", sep="")] <- tmp_new[, i] - mean(tmp_new[, i])
+  
+  
+}
+
+
+
+
+# features for standardization
+
+for(i in 1:ncol(tmp_new)){
+  
+  tmp_features[, paste0(names(tmp_new)[i], "_zscore")] <- ((tmp_new[, i] - mean(tmp_new[, i])) / sd(tmp_new[, i]))
+  
+  
+}
+
+###########################################################################################################
+
+
+# Take interaction features from feature importance from various different algos (Eg : 20 from each)
+
+# Interaction features
+
+
+int_col <- c()
+
+tmp_int <- tmp_new[ , int_col]
+
+
+
+# create + interaction features
+
+
+for (i in 1:ncol(tmp_int)) {
+  
+  for (j in (i + 1) : (ncol(tmp_int) + 1)) {
     
-    for(eta in c(0.3, 0.2, 0.1)){
-      
-      # train
-      param <- list(objective = "multi:softprob",
-                    
-                    eval_metric = "mlogloss",
-                    
-                    num_class = numberOfClasses,
-                    
-                    max_depth = depth ,
-                    
-                    eta = eta
-                    )
-      
-      
-      clf <- xgb.train(params = param, data = dtrain, watchlist = watchlist, nrounds = rounds,
-                       
-                       verbose = 1, maximize = T, nthread = 2)
-      gc()
-      
-      
-      xgb.save(clf, paste0("clf", "_", rounds, "_",depth, "_", eta) )
-      
-      #scoring to be done -- issues with function scoring
-      
-    }     
+    var.x <- colnames(tmp_int)[i]
+    
+    var.y <- colnames(tmp_int)[j]
+    
+    var.new <- paste0(var.x, '_plus_', var.y)
+    
+    tmp_int[, paste0(var.new)] <- tmp_int[, i] + tmp_int[, j]
+    
   }
-}  
+}
 
 
-Time_Taken <- Sys.time() - start
 
 
-##after 900 rounds it increases then falls off check it`s behaviour further
+# create - interaction features
 
-# NOT USING THE SUBMISSION FUNCTION PRED FUNCTION FOR NOW 11-10-2015
 
-#submit(clf, test, "1172015.csv")
+for (i in 1:ncol(tmp_int)) {
+  
+  for (j in (i + 1) : (ncol(tmp_int) + 1)) {
+    
+    var.x <- colnames(tmp_int)[i]
+    
+    var.y <- colnames(tmp_int)[j]
+    
+    var.new <- paste0(var.x, '_minus_', var.y)
+    
+    tmp_int[, paste0(var.new)] <- tmp_int[, i] - tmp_int[, j]
+    
+  }
+}
 
-pred <- predict(clf, data.matrix(test[, feature.names])) 
 
-pred <- matrix(pred, nrow=38, ncol=length(pred)/38) #there are total 38 classes 
 
-pred <-  data.frame(t(pred))
 
-sample <- read_csv("D:/kaggle/walmart_seg/Data/sample_submission.csv") 
 
-cnames <- names(sample)[2:ncol(sample)] 
+# create * interaction features
 
-names(pred) <- cnames
 
-submission <- cbind.data.frame(VisitNumber = visit_num , pred) 
+for (i in 1:ncol(tmp_int)) {
+  
+  for (j in (i + 1) : (ncol(tmp_int) + 1)) {
+    
+    var.x <- colnames(tmp_int)[i]
+    
+    var.y <- colnames(tmp_int)[j]
+    
+    var.new <- paste0(var.x, '_mult_', var.y)
+    
+    tmp_int[, paste0(var.new)] <- tmp_int[, i] * tmp_int[, j]
+    
+  }
+}
 
-submission <- setDT(submission)
 
-submission <- (submission[ , lapply(.SD, mean), by = VisitNumber])
 
-write_csv(submission, "D:/kaggle/walmart_seg/submission/11102015.csv")
 
-####################################################################################################################
 
-#save and retrain model later
+# create / interaction features
 
-ptrain <- predict(clf, dtrain, outputmargin = T)
 
-setinfo(dtrain, "base_margin", ptrain)
+for (i in 1:ncol(tmp_int)) {
+  
+  for (j in (i + 1) : (ncol(tmp_int) + 1)) {
+    
+    var.x <- colnames(tmp_int)[i]
+    
+    var.y <- colnames(tmp_int)[j]
+    
+    var.new <- paste0(var.x, '_divide_', var.y)
+    
+    tmp_int[, paste0(var.new)] <- tmp_int[, i] / tmp_int[, j]
+    
+  }
+}
 
-clf_extra <- xgboost(params = param, data = dtrain, nround = 1500)
+
+
+tmp_int <- tmp_int[, -int_col]
+
+################################################################################################
+
+
+# Dealing with text data
+
+# Tried methods : 1. Length of string (nchar)
+
+#               : 2. number of words
+
+#               : 3. N-Grams
+
+
+# create a data frame with columns being of strings (character type)
+
+# create a new tmp df with raw features
+
+# these changes introduced NA`s in dval N dtrain 
+
+# solution : remove NA`s before combining 
+
+tmp_1 <- read_csv("D:/kaggle/walmart_seg/Data/train.csv")[-1]
+
+tmp_2 <- read_csv("D:/kaggle/walmart_seg/Data/test.csv")
+
+tmp <- rbind(tmp_1, tmp_2)
+
+tmp_str <- data.frame((tmp[ , "DepartmentDescription"]))
+
+
+tmp_str[is.na(tmp_str)] <- 0
+
+
+names(tmp_str) = ("Dept_Desc")
+
+
+
+
+#convert columns from character to factors in a data frame
+
+# code sourced from Stack Overflow
+
+
+i <- sapply(tmp_str, is.factor)
+
+
+tmp_str[i] <- lapply(tmp_str[i], as.character)
+
+
+tmp_str[, paste0("str_len")] <-  nchar(x = tmp_str$Dept_Desc) 
+
+
+
+# calculate number of words
+
+
+tmp_str$Dept_Desc <- gsub(' {2,}',' ', tmp_str$Dept_Desc)
+
+lnth <- rep(0, nrow(tmp_str))
+
+for(i in 1:nrow(tmp_str)) {
+  
+  lnth[i] <-  length(strsplit(tmp_str$Dept_Desc[i],' ')[[1]])
+  
+}
+
+tmp_str$num_wrd <- lnth
+
+
+
+# calculate N-Grams of words
+
+
+rm(tmp); rm(tmp_1); rm(tmp_2)
+
+############################################################################################################
+
+
+# combine all temporarily created df`s into tmp_new in a single piece of code  
+
+# tmp_count
+
+# tmp_dummy
+
+# tmp_features
+
+# tmp_int
+
+# tmp_str
+
+
+#tmp_new = cbind.data.frame(tmp_new, tmp_dummy)
+
+
+tmp_new = cbind.data.frame(tmp, tmp_count, tmp_dummy)
+
+tmp_new = cbind.data.frame(tmp_new, tmp_features)
+
+tmp_new = cbind.data.frame(tmp_new, tmp_str)
+
+
+gc()
+
+#remove unwanted df
+
+rm(nms); rm(nms_df); rm(sum1); rm(tmp_count); rm(tmp_factors); rm(tmp1); rm(tmp_features) 
+
+rm(tmp); rm(tmp_str); rm(train); rm(test)
+
+#rm(tmp_new_cpy); rm(tmp_cpy)
+
+gc()
+
+#seperate train from test
+
+train <- tmp_new[c(1:647054),]
+
+test <- tmp_new[c(647055:1300700),]
+
+dim(train); dim(test)
+
+gc()
+
+#rm(tmp_new); rm(tmp_str)
+
+#gc()
+
+##############################################################################################################
+
+#check for new ways to impute NA
+
+# options : 1. PLot the distributions of each column
+
+#           2. Plot the distribution of NA
+
+#           3. Impute by mean, KNN ( both are found in caret ) **** should try for this competition *** 
+
+train[is.na(train)] <- 0
+
+gc()
+
+test[is.na(test)] <- 0
+
+gc()
